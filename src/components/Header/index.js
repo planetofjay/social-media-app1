c212c32c37d10d572d7113361ccb2bbf0a343ebf
PlanetofJay <jae44dv@gmail.com>
2023-02-21 19:55:59 -0500

    Header with scss, define variables.scss

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,10 @@
 import { appName } from './../../includes/variables';
 import { ImNewspaper } from 'react-icons/im'
+import './styles.scss';
 
 export default function Header() {
   return (
-    <header>
+    <header className='main'>
       <ImNewspaper />
       <h1>{appName}</h1>
     </header>
