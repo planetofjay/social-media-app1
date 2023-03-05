66ade34f1b1c9346e199039a623ce8454070394a
PlanetofJay <jae44dv@gmail.com>
2023-03-05 16:09:13 -0500

    add basic styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,17 @@ import { appName } from './../../includes/variables';
 import { ImNewspaper } from 'react-icons/im'
 import './styles.scss';
 
+import MainMenu from '../MainMenu';
+
 export default function Header() {
   return (
-
-    <header className='main'>
-      <ImNewspaper />
-      <h1>{appName}</h1>
-    </header>
     
+    <>
+      <header className='main'>
+        <ImNewspaper />
+        <h1>{appName}</h1>
+      </header>
+      <MainMenu />
+    </>
   );
 }
