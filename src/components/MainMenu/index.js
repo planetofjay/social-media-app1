a1fca98517213f4eb82b4e3bccd9a489f2079f85
PlanetofJay <jae44dv@gmail.com>
2023-03-06 16:26:18 -0500

    use NavLink instead of Link

--- src/components/MainMenu/index.js
+++ src/components/MainMenu/index.js
@@ -1,15 +1,15 @@
 import './styles.scss';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function MainMenu() {
   
   return (
 
     <nav className='main'>
-      <Link to='/'>Home</Link>
-      <Link to='/posts'>Posts</Link>
-      <Link to='/preferences'>Preferences</Link>
-      <Link to='/about-us'>About Us</Link>
+      <NavLink to='/'>Home</NavLink>
+      <NavLink to='/posts'>Posts</NavLink>
+      <NavLink to='/preferences'>Preferences</NavLink>
+      <NavLink to='/about-us'>About Us</NavLink>
     </nav>
 
   );
