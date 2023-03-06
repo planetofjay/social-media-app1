a555d35748d389dd46904f160cf0c5892c853198
PlanetofJay <jae44dv@gmail.com>
2023-03-06 15:03:51 -0500

    create pages folder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import Header from "./components/Header";
-import Posts from "./components/Posts";
 import Footer from "./components/Footer";
-import Form from "./components/Form";
-import Settings from "./components/Settings";
 
 export default function App() {
 
   return (
     <>
       <Header />
-      <Posts />
-      <Form />
-      <Settings />
       <Footer />
     </>
   );
---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,12 @@
+export default function Homepage() {
+
+  return (
+    <main>
+      <h1>Welcome to My App</h1>
+      <div>
+        Home page content goes here.
+      </div>
+    </main>
+
+  );
+}
