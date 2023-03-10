9e6bb653b1d0c44f35e93bf34335efa36fd4b08b
PlanetofJay <jae44dv@gmail.com>
2023-03-10 00:32:51 -0500

    update

--- src/pages/PostItemPage/index.js
+++ src/pages/PostItemPage/index.js
@@ -1,6 +1,7 @@
 import PageContainer from "../../components/PageContainer";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import NotFoundPage from "../NotFoundPage";
 
 export default function PostItemPage() {
 
@@ -8,6 +9,10 @@ export default function PostItemPage() {
   const post = useSelector(
     (state) => state.post.posts.find((post) => post.id === params.id)
   );
+
+  if(!post) {
+    return <NotFoundPage />
+  }
   
   return (
 
