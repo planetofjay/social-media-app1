ea3778d2096fc5558fb9fda2afe616348cfd0f6e
PlanetofJay <jae44dv@gmail.com>
2023-03-20 13:30:11 -0400

    test try and catch

--- src/database/read.js
+++ src/database/read.js
@@ -3,7 +3,22 @@ import { db } from "./config";
 
 export async function load() {
 
-    return 'ok';
+  try {
+    const querySnapshot = await getDocs(collection(db, "posts"));
+  
+    const data = [];
+    querySnapshot.forEach((doc) => {
+      data.push({
+        ...doc.data(),
+        id: doc.id
+      });
+    });
+    
+    return data;
+  }
+  catch(error) {
+    throw 'Failed to load the database.';
+  }
 
 }
 
