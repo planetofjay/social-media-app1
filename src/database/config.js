8561c2fe0712dc8622af760c2ccbb08fe6de395f
PlanetofJay <jae44dv@gmail.com>
2023-03-20 12:52:34 -0400

    test firestore

--- src/database/config.js
+++ src/database/config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -12,3 +13,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
--- src/database/read.js
+++ src/database/read.js
@@ -1,6 +1,23 @@
+import { collection, getDocs } from "firebase/firestore"; 
+import { db } from "./config";
+
 export function load() {
 
   console.log('Loading...');
+
+  const dbCollection = collection(db, "posts");
+  getDocs(dbCollection)
+    .then((querySnapshot) => {
+      console.log('Query Snapshot:', querySnapshot);
+    })
+    .catch((error) => {
+      console.log('Error:', error);
+    });
+
+  // const querySnapshot = await getDocs(collection(db, "users"));
+  // querySnapshot.forEach((doc) => {
+  //   console.log(`${doc.id} => ${doc.data()}`);
+  // });
 
 }
 
