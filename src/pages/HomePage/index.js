535ce2c0f6bf13a875d620e16da705e9e761a154
PlanetofJay <jae44dv@gmail.com>
2024-06-11 16:00:08 -0600

    update

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -5,7 +5,7 @@ export default function Homepage() {
 
   return (
 
-    <PageContainer title='Welcome to My App'>
+    <PageContainer title='Personal Stuff'>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <p><strong>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</strong></p>
 
